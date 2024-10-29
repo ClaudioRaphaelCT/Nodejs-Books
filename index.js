dd6e778891cb411c2fbd0d5820ce7ed85079750e
tsbcteixeira <167436439+tsbcteixeira@users.noreply.github.com>
2024-10-29 15:48:12 -0300

    second

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const booksRouters = require('./src/routers/books.router');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+
+// Configuração do CORS
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Substitua pelo domínio do seu frontend
+  }),
+);
 
 // Middleware
 app.use(bodyParser.json());
